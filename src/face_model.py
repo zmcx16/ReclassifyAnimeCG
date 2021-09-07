ea5fcd17a03f0062e4ded89b6035f5a6fa107191
zmcx16 <zmcx16@gmail.com>
2021-09-08 03:13:12 +0800

    remove noused arg

--- src/face_model.py
+++ src/face_model.py
@@ -7,9 +7,9 @@ class FaceModel:
 
     model = None
 
-    def __init__(self, root_path, name):
+    def __init__(self, name):
         if name == "lbpcascade_animeface":
-            self.model = LibCascadeAnimeFace(root_path)
+            self.model = LibCascadeAnimeFace()
 
     def show(self, img_path, args):
         self.model.show(img_path, args)
@@ -17,5 +17,5 @@ class FaceModel:
 
 if __name__ == "__main__":
     root_path = pathlib.Path(__file__).parent.resolve()
-    model = FaceModel(root_path, "lbpcascade_animeface")
+    model = FaceModel("lbpcascade_animeface")
     model.show("I:\\work\\WORK\\ReclassifyAnimeCG\\ReclassifyAnimeCG\\data-sample\\classified-data\\Emilia\\1.jpg", None)
