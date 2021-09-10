601ece7c19a7f8641e3faf69065fd7265f7a25b2
zmcx16 <zmcx16@gmail.com>
2021-09-11 05:17:51 +0800

    add pytorch load dataset sample code

---
+++ src/load_dataset_sampe.py
@@ -0,0 +1,30 @@
+import torch
+import torchvision
+from torchvision import transforms, utils
+import matplotlib.pyplot as plt
+
+img_data = torchvision.datasets.ImageFolder('I:\\work\\WORK\\ReclassifyAnimeCG\\ReclassifyAnimeCG\\data-sample\\classified-data',
+                                            transform=transforms.Compose([
+                                                transforms.Scale(256),
+                                                transforms.CenterCrop(224),
+                                                transforms.ToTensor()])
+                                            )
+
+print(len(img_data))
+data_loader = torch.utils.data.DataLoader(img_data, batch_size=20, shuffle=True)
+print(len(data_loader))
+
+
+def show_batch(imgs):
+    grid = utils.make_grid(imgs,nrow=5)
+    plt.imshow(grid.numpy().transpose((1, 2, 0)))
+    plt.title('Batch from dataloader')
+
+
+for i, (batch_x, batch_y) in enumerate(data_loader):
+    if i < 4:
+        print(i, batch_x.size(), batch_y.size())
+
+        show_batch(batch_x)
+        plt.axis('off')
+        plt.show()
